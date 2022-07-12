2627a0e6b234ec0201f774fb34c52ce78d8391f8
lopasov89 <lopasov89@gmail.com>
2022-07-12 16:24:27 +0400

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
