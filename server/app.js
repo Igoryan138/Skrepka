f4de892699b21de5f60b3ff287d478176c5d40b7
Igor Yakushev <i19931@mail.ru>
2022-07-13 22:27:03 +0800

    add

--- server/app.js
+++ server/app.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require("cors");
-const dbCheck = require('./db/dbCheck') // подключение скрипта проверки соединения с БД
+// const dbCheck = require('./db/dbCheck') // подключение скрипта проверки соединения с БД
 const indexRouter = require('./routes/index');
+const advertisementRouter = require('./routes/advertisementRouter');
+
 
 // ! Инициализируем приложение
 const app = express() // создали экземпляр сервера
 const PORT = process.env.PORT || 3002 // создали константу с портом
-dbCheck() // вызов функции проверки соединения с базоый данных
+// dbCheck() // вызов функции проверки соединения с базоый данных
 
 // ! Подключаем миддлварки
 app.use(cors());
@@ -18,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // ! -->Тут пишем роуты<--
 app.use('/', indexRouter);
+app.use('/advertisement', advertisementRouter)
 
 // ! Начинаем слушать порт для запуска сервера
 app.listen(PORT, () => { // начинаем слушать сервер на указанном порте
