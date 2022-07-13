36038854618a7564d32f83af7643e83b22eb6613
Igor Yakushev <i19931@mail.ru>
2022-07-13 14:29:06 +0800

    profile start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,33 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
-import { Registration, Auth } from './components';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
-import Faq from '../components/Faq/Faq';
-import Main from '../components/Main/Main';
+import Profile from './components/Profile/Profile';
+// import { Registration, Auth } from './components';
+// import Navbar from '../components/Navbar/Navbar';
+// import Footer from '../components/Footer/Footer';
+// import Faq from '../components/Faq/Faq';
+// import Main from '../components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
+      <header>
+        {/* <Navbar /> */}
+      </header>
       <main>
         <Routes>
-          <Route path="/" element={<Main/>}/>
+          {/* <Route path="/" element={<Main/>}/>
           <Route path="registration" element={<Registration/>} />
           <Route path="login" element={<Auth/>} />
-          <Route path="faq" element={<Faq/>} />
+          <Route path="faq" element={<Faq/>} /> */}
+
         </Routes>
+        <Profile />
+
       </main>
-        <Footer />
+      <footer>
+        {/* <Footer /> */}
+      </footer>
     </div>
   );
 }
---
+++ client/src/components/Profile/MyAdvertisements/MyAdvertisements.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MyAdvertisements() {
+  return (
+    <div>
+      <div>
+        Мои объявления
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
