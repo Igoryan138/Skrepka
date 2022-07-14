e1a48d5e0f44aef2d58ba85b8ca93bda88d8808a
BeTaJI <lilysh994@mail.ru>
2022-07-14 17:26:03 +0300

    destroy session - complete

--- server/routes/logOutRouter.js
+++ server/routes/logOutRouter.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
   if(req.session){
-    req.session.destroy()
-
+    req.session.destroy();
+    res.clearCookie(process.env.COOKIE_NAME);
+    res.end();
   }
 })
 
