89ea0991e46f0050d74d159c74aed2c52678db13
BeTaJI <lilysh994@mail.ru>
2022-07-18 19:27:34 +0300

    до мерджа

--- client/src/components/AdvertList/AdvertList.jsx
+++ client/src/components/AdvertList/AdvertList.jsx
@@ -87,14 +87,17 @@ export default function AdvertList() {
      <div className="pagination">
         <nav aria-label="Page navigation example">
           <ul className="pagination justify-content-center">
-            <li className="page-item disabled">
-              <a className="page-link" href="#" tabIndex="-1" aria-disabled="true">Previous</a>
+          <li className="page-item" onClick={ ()=> setPage(page -1)}>
+              <a className="page-link"  >Назад</a>
             </li>
-            <li className="page-item"><a className="page-link" href="#">1</a></li>
-            <li className="page-item"><a className="page-link" href="#">2</a></li>
-            <li className="page-item"><a className="page-link" href="#">3</a></li>
-            <li className="page-item">
-              <a className="page-link" href="#">Next</a>
+
+            {/* {pages.map((el) =><li className="page-item"><a className="page-link" href={el}>1</a></li> */}
+
+            <li className="page-item"><a className="page-link" href={page}>1</a></li>
+
+
+            <li className="page-item" onClick={ ()=> setPage(page +1)}>
+              <a className="page-link"  >Вперед</a>
             </li>
           </ul>
         </nav>
