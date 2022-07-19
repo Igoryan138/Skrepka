194a916ad111fa26e72f5e6a4af680c233d5e4f3
BeTaJI <lilysh994@mail.ru>
2022-07-19 23:27:05 +0300

    add style footer (Прибит но не везде - профиль добавление товара)

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
-export  function Footer() {
+import { Link } from 'react-router-dom'
+import style from './style.module.css'
+export function Footer() {
   return (
-    <footer>
-      Footer
-    </footer>
+    <div className={style.footer}>
+
+      <footer className="py-3">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+
+          <li className="nav-item">
+            <Link to='/' className="nav-link px-2 text-muted">Home</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to='/faq' className="nav-link px-2 text-muted">FAQs</Link>
+          </li>
+
+        </ul>
+        <p className="text-center text-muted">Создано группой програмистов при поддержке Elbrus Bootcamp</p>
+      </footer>
+
+    </div>
+
   )
 }
 
