09c0941c53b1cbf0ce7fdf50ae4a640260c0f028
lopasov89 <lopasov89@gmail.com>
2022-07-20 11:42:11 +0400

    auth reg

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -3,62 +3,63 @@ import { useState } from 'react'
 // import axios from 'axios'
 import { useDispatch } from 'react-redux';
 import { setUser } from '../../redux/actions/user.action'
+import styles from './registr.module.css'
 
 
 
-export  function Registration() {
-  
+export function Registration() {
+
 
   const [input, setInput] = useState({});
   const dispatch = useDispatch();
- 
-  
 
-  function inputHandler(e){
-    setInput((prev) => ({...prev,[e.target.name]: e.target.value})
+
+
+  function inputHandler(e) {
+    setInput((prev) => ({ ...prev, [e.target.name]: e.target.value })
     )
   }
-  
-  function regHandler(event){
-    
-  event.preventDefault()
+
+  function regHandler(event) {
+
+    event.preventDefault()
     dispatch(setUser(input))
-   
-      // setInput('')
+
+    // setInput('')
   }
 
   return (
-    <div className="container">
-    <form  onSubmit={(e)=> regHandler(e)} className="row g-3 needs-validation" >
-  <div className="col-md-4">
-    <label  htmlFor="validationCustom01" className="htmlForm-label">Имя</label>
-    <input  name='firstName'  onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom01" required/>
-  </div>
+    <div className={`${styles.registr} container`}>
+      <form onSubmit={(e) => regHandler(e)} >
+        <div >
+          {/* <label htmlFor="validationCustom01" className="htmlForm-label">Имя</label> */}
+          <input name='firstName' placeholder='Введите имя' onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom01" required />
+        </div>
 
-  <div className="col-md-4">
-    <label  htmlFor="validationCustom02" className="htmlForm-label">Фамилия</label>
-    <input name='lastName'  onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom02" required/>
-  </div>
-  
-  <div className="col-md-3">
-    <label  htmlFor="validationCustom05" className="htmlForm-label">E-mail</label>
-    <input name='email'  onChange={inputHandler} type="email" className="htmlForm-control" id="validationCustom05" placeholder='example: ivan@mail.ru' required/>
-  </div>
+        <div >
+          {/* <label htmlFor="validationCustom02" className="htmlForm-label">Фамилия</label> */}
+          <input name='lastName' placeholder='Введите фамилию' onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom02" required />
+        </div>
 
-  <div className="col-md-4">
-    <label htmlFor="validationCustom02" className="htmlForm-label">Телефон</label>
-    <input name='phone'  onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom02" placeholder='example: 89645553325' required/>
-  </div>
+        <div >
+          {/* <label htmlFor="validationCustom05" className="htmlForm-label">E-mail</label> */}
+          <input name='email' onChange={inputHandler} type="email" className="htmlForm-control" id="validationCustom05" placeholder='E-mail: ivan@mail.ru' required />
+        </div>
 
-  <div className="col-md-4">
-    <label htmlFor="validationCustom02" className="htmlForm-label">Пароль</label>
-    <input name='password'  onChange={inputHandler} type="password" className="htmlForm-control" id="validationCustom02" required/>
-  </div>
+        <div >
+          {/* <label htmlFor="validationCustom02" className="htmlForm-label">Телефон</label> */}
+          <input name='phone' onChange={inputHandler} type="text" className="htmlForm-control" id="validationCustom02" placeholder='Телефон: +7 (999) 999-99-99' required />
+        </div>
 
-  <div className="col-12">
-    <button className="btn btn-primary" type="submit">Регистрация</button>
-  </div>
-</form>
+        <div >
+          {/* <label htmlFor="validationCustom02" className="htmlForm-label">Пароль</label> */}
+          <input name='password' onChange={inputHandler} type="password" placeholder='Придумайте пароль' className="htmlForm-control" id="validationCustom02" required />
+        </div>
+
+        <div className="col-12">
+          <button className="btn btn-primary mt-4" type="submit">Регистрация</button>
+        </div>
+      </form>
     </div>
   )
 }
