8f823c3c50e7b2adeb8ef7101638151376210c9e
lopasov89 <lopasov89@gmail.com>
2022-07-20 10:50:53 +0400

    pravki

--- client/src/components/Profile/MyAdvertisements/MyAdvertItem/MyAdvertItem.jsx
+++ client/src/components/Profile/MyAdvertisements/MyAdvertItem/MyAdvertItem.jsx
@@ -9,14 +9,16 @@ export default function MyAdvertItem({ el }) {
   }
 
   return (
-        <div className={style.card}>
-          <img src={`${process.env.REACT_APP_API_URL}${el.url}`}className="card-img-top" alt="" />
-          <div className={style.card_body}>
-            <Link to={`/add/${el.id}`}>
-              <h5 className="card-title">{el.title}</h5>
-            </Link>
-            <button type="button" onClick={deleteAdvert} className="btn btn-warning">Завершить</button>
-          </div>
-        </div>
+    <div className={style.card}>
+      <Link to={`/add/${el.id}`}>
+        <img src={`${process.env.REACT_APP_API_URL}${el.url}`} className="card-img-top" alt="" />
+      </Link>
+      <div className={style.card_body}>
+        <Link to={`/add/${el.id}`}>
+          <h5 className="card-title">{el.title}</h5>
+        </Link>
+        <button type="button" onClick={deleteAdvert} className="btn btn-info">Завершить</button>
+      </div>
+    </div>
   )
 }
